87af625b7483537579ae37ff14a80d8fa2f4a2d9
lumap <lumapbk@gmail.com>
2020-07-23 17:06:33 +0200

    plzzz

--- src/functions/duration.js
+++ src/functions/duration.js
@@ -6,9 +6,11 @@
 module.exports = function duration(duration) {
     let totalSeconds = duration/1000
     totalSeconds %= 3600;
+    let hours = Math.floor(totalSeconds / 3600) % 24;
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
     minutes=minutes>=10?minutes:"0"+minutes
     seconds=seconds>=10?seconds:"0"+seconds
-    return `${minutes}:${seconds}`
+    return `${hours}:${minutes}:${secondste}`
+    
 }
