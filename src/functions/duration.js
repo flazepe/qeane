6a0150f62161023c572e885e636c17a4ecabd694
lumap <lumapbk@gmail.com>
2020-07-23 17:08:55 +0200

    bug fix

--- src/functions/duration.js
+++ src/functions/duration.js
@@ -5,12 +5,15 @@
  */
 module.exports = function duration(duration) {
     let totalSeconds = duration/1000
-    totalSeconds %= 3600;
     let hours = Math.floor(totalSeconds / 3600) % 24;
+    totalSeconds %= 3600;
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
     minutes=minutes>=10?minutes:"0"+minutes
     seconds=seconds>=10?seconds:"0"+seconds
+    if (hours) {
     return `${hours}:${minutes}:${seconds}`
-    
+    } else {
+        return `${minutes}:${seconds}`
+    }
 }
