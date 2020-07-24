d25a6a1c3390379296d31593854773d74381160f
lumap <lumapbk@gmail.com>
2020-07-24 13:32:48 +0200

    plz

--- src/commands/pat.js
+++ src/commands/pat.js
@@ -3,8 +3,11 @@ module.exports = {
     category: "fun",
     async execute (client,msg) {
         require('axios').get('https://weebs4life.ga/api/pat').then(res => {
-            let member = msg.mentions.members.first() || client.functions.findByID(msg.guild,msg.args.join(' ')) || msg.guild.me
-         let title = `${msg.author.tag} pats ${member.user.tag}`
+            let member;
+            if (msg.args.join(' ')) {
+                member = msg.mentions.members.first() || client.functions.findByID(msg.guild,msg.args.join(' '))
+              } else {member = {user: client.user}}
+            let title = `${msg.author.tag} pats ${member.user.tag}`
             msg.reply("",{embed:{image: {url: res.data.url}, title: title}})})
     }
 }
--- src/commands/slap.js
+++ src/commands/slap.js
@@ -3,7 +3,10 @@ module.exports = {
     category: "fun",
     async execute (client,msg) {
         require('axios').get('https://weebs4life.ga/api/slap').then(res => {
-            let member = msg.mentions.members.first() || client.functions.findByID(msg.guild,msg.args.join(' ')) || msg.guild.me
+            let member;
+            if (msg.args.join(' ')) {
+                member = msg.mentions.members.first() || client.functions.findByID(msg.guild,msg.args.join(' '))
+              } else {member = {user: client.user}}
          let title = `${msg.author.tag} slaps ${member.user.tag}`
             msg.reply("",{embed:{image: {url: res.data.url}, title: title}})})
     }
