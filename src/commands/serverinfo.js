7d270cb4ec54580bd12ac18345dfdc4a80103748
lumap <lumapbk@gmail.com>
2020-07-27 13:59:40 +0200

    fix

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -30,7 +30,6 @@ module.exports = {
           {name: "Server ID", value: guild.id, inline: true},
           {name: "Server region", value: guild.region, inline: true},
           {name: "Member count", value: guild.memberCount, inline: true},
-          {name: "Members stats", value: `<:online:707912962605842482> ${members.online}, <:idle:707912947271335937> ${members.idle}, <:dnd:707912919635198064> ${members.dnd}, <:offline:707912934763790378> ${members.offline}, Unknown ${members.unknow}\nHumans: ${members.humans}, Bots: ${members.bots}`, inline: true},
           {name: "Large server", value: guild.large?"Yes":"No", inline: true},
           {name: "Default notification level", value: guild.defaultmsgNotifications, inline: true},
           {name: "Owner", value: owner.nickname?`${owner.nickname} (${owner.user.tag})`:owner.user.tag, inline: true},
@@ -38,9 +37,11 @@ module.exports = {
           {name: "Role count", value: guild.roles.cache.size, inline: true}, 
           {name: "Chanels stats", value: `Text: ${channels.text}, Voice: ${channels.voice}, Categories, ${channels.categories}, Unknown: ${channels.unknow}`, inline: true},
           {name: "Emoji count", value: guild.emojis.cache.size ,inline: true},
+          {name: "Emoji list", value: emojilist?emojilist.length>1024?`Too much emojis to display. Try doing \`\`serverinfo emoji list\`\``:emojilist:"No emojis", inline: true},
           {name: "Channel list", value: channellist.length>1024?`Too much channels to display. Try doing \`\`serverinfo channel list\`\``:channellist, inline: channellist.length>1024?true:false},  
           {name: "Role list", value: rolelist.length>1024?`Too much roles to display. Try doing \`\`serverinfo role list\`\``:rolelist, inline: rolelist.length>1024?true:false}
       ]
+
       if (args.join(' ')) {
         args=args.join(' ').toLowerCase()
         for (let i = 0;i<fields.length;i++) {
