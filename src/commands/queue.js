924bcaa97cbab283c85e0fbe76212a7a1f67fa59
lumap <lumapbk@gmail.com>
2020-07-27 13:35:01 +0200

    taf what hav u done

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -14,23 +14,7 @@ module.exports = {
       if (!output[1]) output[1]='Nothing'
       
       const Discord = require('discord.js')
-
-      const nlr = new Discord.MessageEmbed()
-      .setColor('RANDOM')
-      .setAuthor('Server queue')
-      .setDescription(`**__Now playing :__** \n[${player.queue[0].title}](https://nlradio.xyz/)\n\n**__Incoming :__** \n${output.join(' ')}`)
-      if (player.queue.length > 11) return nlr.setFooter(`${player.queue.length-11} more songs...`)
-
-      const nlrfr = new Discord.MessageEmbed()
-      .setColor('RANDOM')
-      .setAuthor('Server queue')
-      .setDescription(`**__Now playing :__** \n[${player.queue[0].title}](https://nlradio.xyz/fr/)\n\n**__Incoming :__** \n${output.join(' ')}`)
-      if (player.queue.length > 11) return nlr.setFooter(`${player.queue.length-11} more songs...`)
-
-      if (player.queue[0].uri === 'https://admin.nlradio.xyz/radio/8010/nlr.mp3') return msg.channel.send("",nlr)
-      if (player.queue[0].uri === 'https://admin.nlradio.xyz/radio/8000/french/nlr.mp3') return msg.channel.send("",nlrfr)
       let queueemb = new Discord.MessageEmbed()
-
       .setColor('RANDOM')
       .setAuthor('Server queue')
       .setDescription(`**__Now playing :__** \n[${player.queue[0].title}](${player.queue[0].uri})\n\n**__Incoming :__** \n${output.join(' ')}`)
