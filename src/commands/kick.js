482195cfa1f902c1d0afb5d8fffe317c723f9641
lumap <lumapbk@gmail.com>
2020-07-29 08:59:35 +0200

    fix

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -2,17 +2,16 @@ module.exports = {
     name: 'kick',
     aliases: ['k'],
     category: "moderation",
-    async execute(client,msg,args) {
-        if(!msg.member.permissions.has("KICK_MEMBERS")) return msg.reply("Woops, you can't kick members!")
-        if(!msg.guild.me.permissions.has("KICK_MEMBERS")) return msg.reply("Woops, I can't kick members!")
+    async execute(client, msg) {
+        if (!msg.member.permissions.has("KICK_MEMBERS")) return msg.reply("Woops, you can't kick members!")
+        if (!msg.guild.me.permissions.has("KICK_MEMBERS")) return msg.reply("Woops, I can't kick members!")
         if (!msg.args.join(' ')) return msg.reply("Please provide a user to ban! (mention or user id)")
-        let member = msg.mentions.members.first() || client.functions.findByID(msg.guild,msg.args.join(' '))
-        let reason = args.slice(1).join(' ')
-        if (!reason) reason = 'No reason provided'
-    if (!member) return msg.channel.send('Woops,  user not found!')
-    if (member.user.id === client.ownerID) return msg.reply("I can't kick my dev!")
-    member.user.send(`You have been kicked from **${msg.guild.name}** by **${msg.author.tag}**`)
-    member.kick({reason: reason})
-    msg.reply("Member succesfully kicked!")
+        let member = msg.mentions.members.first() || client.functions.findByID(msg.guild, msg.args.join(' '))
+        let reason = msg.args.slice(1).join(' ') || 'No reason provided'
+        if (!member) return msg.channel.send('Woops,  user not found!')
+        if (member.user.id === client.ownerID) return msg.reply("I can't kick my dev!")
+        member.user.send(`You have been kicked from **${msg.guild.name}** by **${msg.author.tag}**`)
+        member.kick({ reason: reason })
+        msg.reply("Member succesfully kicked!")
     }
 }
--- src/commands/play.js
+++ src/commands/play.js
@@ -3,6 +3,7 @@ module.exports = {
     aliases: ['p'],
     category: "music",
     async execute(client, msg) {
+        return msg.reply("Sorry, but music commands are not avaible for now :(")
         const { channel } = msg.member.voice
         if (!channel) return msg.reply("Woops, you doesn't seem to be in a voice channel!")
         await channel.fetch()
