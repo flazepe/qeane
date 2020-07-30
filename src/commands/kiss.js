479059d4bf84441e22e3ff053962c75affb3c11b
lumap <lumapbk@gmail.com>
2020-07-30 12:32:44 +0200

    plz senpai

--- src/commands/kiss.js
+++ src/commands/kiss.js
@@ -2,8 +2,9 @@ module.exports = {
     name: 'kiss',
     category: "fun",
     async execute(client, msg) {
-        require('axios').get('https://weebs4life.ga/api/kiss').then(res => {
+        require('axios').get('https://weebs4life.ga/api/kiss').then(async res => {
             let member = msg.mentions.members.first() || client.functions.findByID(msg.guild, msg.args.join(' ')) || msg.guild.me
+            await member.user.fetch()
             let title = `${msg.author.tag} kisses ${member.user.tag}`
             msg.reply("", { embed: { image: { url: res.data.url }, title: title } })
         })
