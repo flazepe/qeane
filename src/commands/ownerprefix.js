a7b699724cdae5170acd5b29be140f63e685abb7
Tafina <tafina50@gmail.com>
2020-07-30 14:55:30 +0300

    Create ownerprefix.js

---
+++ src/commands/ownerprefix.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "restart",
+    ownerOnly: true,
+    category: "owner",
+    async execute(_client,msg) {
+
+        if (!msg.args.join(' ')) return msg.channel.send('Please provide args! To know this server prefix, just mention me!')
+        client.db.set("prefix."+msg.guild.id,msg.args.join(' '))
+        msg.channel.send('Changed my prefix to **'+msg.args.join(' ')+'**')
+        
+    }
+}
