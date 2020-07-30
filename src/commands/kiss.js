5841ce0ce4f39cdf6a9e36e4b7e0984af448dc52
lumap <lumapbk@gmail.com>
2020-07-30 12:35:27 +0200

    fixed

--- src/commands/kiss.js
+++ src/commands/kiss.js
@@ -2,9 +2,12 @@ module.exports = {
     name: 'kiss',
     category: "fun",
     async execute(client, msg) {
-        require('axios').get('https://weebs4life.ga/api/kiss').then(async res => {
-            let member = msg.mentions.members.first() || client.functions.findByID(msg.guild, msg.args.join(' ')) || msg.guild.me
-            let title = `${msg.author.tag} kisses ${member.user.tag}`
+        require('axios').get('https://weebs4life.ga/api/kiss').then(res => {
+            let member;
+            if (msg.args.join(' ')) {
+                member = msg.mentions.members.first() || client.functions.findByID(msg.guild, msg.args.join(' '))
+            } else { member = { user: client.user } }
+            let title = `${msg.author.tag} hugs ${member.user.tag}`
             msg.reply("", { embed: { image: { url: res.data.url }, title: title } })
         })
     }
