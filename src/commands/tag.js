33278053b87fa28da84fb62687b817b243ac7f63
lumap <lumapbk@gmail.com>
2020-07-31 14:56:55 +0200

    bug fix

--- src/commands/tag.js
+++ src/commands/tag.js
@@ -17,12 +17,14 @@ module.exports = {
                 }
                 client.db.set(`tags.${msg.guild.id}.${msg.args[0]}`, content)
                 msg.reply('Tag created!')
+                break;
             case "delete":
                 msg.args.slice(1)
                 if (!msg.args[0]) return msg.reply("You need to provide a tag name to delete it!!")
                 if (!client.db.has(`tags.${msg.guild.id}.${msg.args[0]}`)) return msg.reply("This tag don't exist!")
                 client.db.delete(`tags.${msg.guild.id}.${msg.args[0]}`)
                 msg.reply('Tag deleted!')
+                break;
             case "list":
                 let tags = client.db.get(`tags.${msg.guild.id}`)
                 let list = Object.keys(tags).join(', ')
@@ -33,8 +35,10 @@ module.exports = {
                         title: "Tag list"
                     }
                 })
+                break;
             default:
-                return msg.reply("Usage: `tag <create/delete/list>`")
+                msg.reply("Usage: `tag <create/delete/list>`")
+                break;
         }
     }
 }
