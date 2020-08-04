e81c2a4027a2be23ac8917306b2d22fff8002509
Lumap <lmp@lumap.localdomain>
2020-08-05 01:22:48 +0200

    j

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     category: "info",
     async execute(client, msg) {
-        msg.reply(client.languages.get(msg.guild.language.)commands.ping.pong
+        msg.reply(client.languages.get(msg.guild.language).commands.ping.pong
             .replace("{0}", client.ws.ping))
     }
 }
