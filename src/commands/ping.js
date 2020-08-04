93497cd4b823595bc1dd4e2c6cf86e33b614644d
Lumap <lmp@lumap.localdomain>
2020-08-05 01:19:32 +0200

    h

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,5 +4,5 @@ module.exports = {
     async execute(client, msg) {
         msg.reply(client.languages.get(msg.guild.id).commands.ping.pong
             .replace("{0}", client.ws.ping))
-    },
+    }
 }
