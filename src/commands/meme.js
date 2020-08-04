b0ea1bab653d1793e300c34f5700e47f71d0e998
Lumap <lmp@lumap.localdomain>
2020-08-04 13:40:35 +0200

    ow

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -1,15 +1,18 @@
 module.exports = {
-	name: 'meme',
-  aliases: ['memes'], 
+  name: 'meme',
+  aliases: ['memes'],
   category: "fun",
-	async execute (client,msg) {
-              const meme = await client.ksoft.images.meme({nsfw: false});
-              msg.reply("",{embed:{
-                color: client.functions.randomColor(),
-                image: {url: meme.url},
-                description: `[Link to the image](${meme.url})`
-              }}) 
-                 
-	}
+  async execute(client, msg) {
+    const meme = await client.ksoft.images.meme({ nsfw: false });
+    console.log(meme)
+    msg.reply("", {
+      embed: {
+        color: client.functions.randomColor(),
+        image: { url: meme.url },
+        description: `[Link to the image](${meme.url})`
+      }
+    })
+
+  }
 };
 
