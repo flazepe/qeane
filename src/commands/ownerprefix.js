670e897a19f8ca8602130b93e9db28ac9f963559
Lumap <lmp@lumap.localdomain>
2020-08-04 23:43:11 +0200

    a

--- src/commands/ownerprefix.js
+++ src/commands/ownerprefix.js
@@ -3,7 +3,7 @@ module.exports = {
     ownerOnly: true,
     category: "owner",
     async execute(client, msg) {
-        let str = client.languages.get(msg.guild.id).commands.prefix
+        let str = client.languages.get(msg.guild.language).commands.prefix
         if (!msg.args.join(' ')) return msg.reply(str.noArgs)
         client.db.set("prefix." + msg.guild.id, msg.args.join(' '))
         msg.reply(str.success
