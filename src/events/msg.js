bd51b8aabf4c31e149258b2cd572a18126636440
Lumap <lmp@lumap.localdomain>
2020-08-04 17:06:46 +0200

    e

--- src/events/msg.js
+++ src/events/msg.js
@@ -24,8 +24,29 @@ module.exports = async (client, msg, cooldown) => {
   const commandName = msg.content.slice(prefix.length).trim().split(' ')[0].toLowerCase()
   msg.args = msg.content.slice(prefix.length).trim().split(' ').slice(1).join(' ').trim().split(' ')
   if (client.db.has(`tags.${msg.guild.id}.${commandName}`)) {
-    await require('./tags')(client, msg)
-    return;
+    if (!client.config.ownerID.includes(msg.author.id)) {
+      if (!msg.member.permissions.has("MANAGE_GUILD")) return;
+    }
+    if (msg.mentions.members.first()) {
+      if (msg.args.contains("-d")) msg.delete()
+      let x = await msg.reply("", {
+        embed: {
+          description: client.db.get(`tags.${msg.guild.id}.${commandName}`),
+          color: 0xe74c3c
+        }
+      })
+      x.edit(`<@!${msg.mentions.members.first().user.id}>`, {
+        embed: x.embeds[0]
+      })
+    } else {
+      if (msg.args.contains("-d")) msg.delete()
+      msg.reply("", {
+        embed: {
+          description: client.db.get(`tags.${msg.guild.id}.${commandName}`),
+          color: 0xe74c3c
+        }
+      })
+    }
   }
   let str = client.languages.get(language)
   const command = client.commands.get(`str.commandNames.${commandName}`) || client.commands.get(`str.aliases.${commandName}`)
