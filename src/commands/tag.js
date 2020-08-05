e160ef7c63de32b818efe15826fd85c06c491d28
Lumap <lmp@lumap.localdomain>
2020-08-05 14:31:52 +0200

    e

--- src/commands/tag.js
+++ src/commands/tag.js
@@ -2,8 +2,6 @@ module.exports = {
     name: "tag",
     category: "moderation",
     async execute(client, msg) {
-        let vote = client.db.get(`votes.${msg.author.id}`)
-        if (!vote || vote < Date.now()) return msg.reply("In order to use the tags commands, you need to vote for me! https://top.gg/bot/727163097026003004/vote")
         if (!msg.member.permissions.has("MANAGE_GUILD")) return msg.reply("You need the Manage Server permission in order to do this command!")
         switch (msg.args[0]) {
             case "create":
