f339d99c32a4177f683d8595aff0e9c0cd0e1ec7
Lumap <lmp@lumap.localdomain>
2020-08-05 14:37:28 +0200

    g

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -5,7 +5,7 @@ module.exports = {
         let str = client.languages.get(msg.guild.language).commands.loop
         let musicStr = client.languages.get(msg.guild.language).music
         let serverQueue = client.queue.get(msg.guild.id)
-        if (!serverQueue) return msg.reply(musicStrqueueEmpty)
+        if (!serverQueue) return msg.reply(musicStr.queueEmpty)
         if (!msg.member.voice.channel) return msg.reply(musicStr.noVc)
         let vc = await msg.member.voice.channel.fetch()
         if (serverQueue.voiceChannel.id !== vc.id) return msg.reply(musicStr.notSameVc)
