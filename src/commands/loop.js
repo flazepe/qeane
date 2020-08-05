093b110a878818f229c8cc517b152ba614ab9f39
Lumap <lmp@lumap.localdomain>
2020-08-05 14:35:02 +0200

    h

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -2,25 +2,26 @@ module.exports = {
     name: "loop",
     category: "music",
     async execute(client, msg) {
-        let str = client.languages.get(msg.guild.language)
+        let str = client.languages.get(msg.guild.language).commands.loop
+        let musicStr = client.languages.get(msg.guold.language).music
         let serverQueue = client.queue.get(msg.guild.id)
-        if (!serverQueue) return msg.reply(str.music.queueEmpty)
-        if (!msg.member.voice.channel) return msg.reply(str.music.noVc)
+        if (!serverQueue) return msg.reply(musicStrqueueEmpty)
+        if (!msg.member.voice.channel) return msg.reply(musicStr.noVc)
         let vc = await msg.member.voice.channel.fetch()
-        if (serverQueue.voiceChannel.id !== vc.id) return msg.reply(str.music.notSameVc)
+        if (serverQueue.voiceChannel.id !== vc.id) return msg.reply(musicStr.notSameVc)
         let type = msg.args[0]
-        let types = [str.commands.loop.types.queue, str.commands.loop.types.track, str.commands.loop.types.disable]
-        if (!types.includes(type.toLowerCase())) return msg.reply(str.commands.loop.usage)
+        let types = [str.queue, str.types.track, str.disable]
+        if (!types.includes(type.toLowerCase())) return msg.reply(strp.usage)
         switch (msg.args[0]) {
-            case str.commands.loop.types.queue:
+            case str.types.queue:
                 serverQueue.loopType = 2
                 msg.reply("The queue will now loop!")
                 return;
-            case str.commands.loop.types.track:
+            case str.types.track:
                 serverQueue.loopType = 1
                 msg.reply("The current track will now repeat!")
                 return;
-            case str.commands.loop.types.disable:
+            case str.types.disable:
                 serverQueue.loopType = 0
                 msg.reply("Loop disabled!")
                 return;
