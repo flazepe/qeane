a56ef736e3b650cf3d3eda3cf521fac1eab5476c
Lumap <lmp@lumap.localdomain>
2020-08-06 14:45:39 +0200

    dbl

--- src/setup/dbl.js
+++ src/setup/dbl.js
@@ -9,6 +9,11 @@ module.exports = (client) => {
         client.db.set(`votes.${vote.user}`, Date.now() + 43200000)
         client.users.fetch(vote.user).then(u => { u.send("Thanks for voting for Qeane!") })
     })
+    dbl.postStats(client.guilds.cache.size, client.ws.shards.id, client.ws.shards.size);
+    setInterval(() => {
+        dbl.postStats(client.guilds.cache.size, client.ws.shards.id, client.ws.shards.size);
+    }, 1800000);
+
     client.dbl = dbl
 
     console.log('==SETUP== dbl succesfully loaded!')
