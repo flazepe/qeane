9e1d9df0eb8d2a95d6714dd0810d6fa03e975923
Lumap <lmp@lumap.localdomain>
2020-08-07 19:27:45 +0200

    h

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -12,10 +12,10 @@ module.exports = {
         }
         if (!c) return msg.reply(str.noCommand);
         const command = client.commands.get(c.name)
-        delete require.cache[require.resolve(`./${command.name}.js`)];
-        client.commands.delete(command.name)
-        client.commands.set(command.name, require(`./${command.name}.js`))
+        delete require.cache[require.resolve(`./${c.name}.js`)];
+        client.commands.delete(c.name)
+        client.commands.set(command.name, require(`./${c.name}.js`))
         msg.reply(str.success
-            .replace("{0}", command.name))
+            .replace("{0}", c.name))
     }
 }
