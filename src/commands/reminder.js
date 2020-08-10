f4e2dde67d6ed753dc8970ca7af72d103066a339
Lumap <lmp@lumap.localdomain>
2020-08-10 16:20:43 +0200

    h

--- src/commands/reminder.js
+++ src/commands/reminder.js
@@ -4,6 +4,7 @@ module.exports = {
     async execute(client, msg) {
         if (!msg.args[1]) return msg.reply("Usage: ``reminder <time> <something>``")
         let timestamp = require('timestamp-to-ms')(msg.args[0])
-        client.db.push('reminders', [{ timelimit: timestamp + Date.now(), channel: msg.channel, user: msg.author, text: args.slice(2).join(' ') }])
+        client.db.push('reminders', [{ timelimit: timestamp + Date.now(), channel: msg.channel, user: msg.author, text: msg.args.slice(2).join(' ') }])
+        msg.reply("done")
     }
 }
