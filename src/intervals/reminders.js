ed6cc1982c2d3b62343f1d82e7537faf2ff3f38b
Lumap <lmp@lumap.localdomain>
2020-08-10 16:26:40 +0200

    h

--- src/intervals/reminders.js
+++ src/intervals/reminders.js
@@ -1,6 +1,7 @@
 module.exports = (client) => {
     setInterval(() => {
         client.db.get('reminders').forEach(r => {
+            r = r[0]
             if (!r) return;
             if (r.timelimit < Date.now()) {
                 r.channel.send(`<@!${r.user.id}>, you wanted me to remind you: ${r.text}`)
